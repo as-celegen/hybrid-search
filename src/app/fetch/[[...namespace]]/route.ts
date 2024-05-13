1d452c533ce0de558fc21ea91ef86854483b075e
as-celegen <ahmedselimcelegen@gmail.com>
2024-05-13 14:49:58 +0300

    Fetch endpoint returns semantic index values

--- src/app/fetch/[[...namespace]]/route.ts
+++ src/app/fetch/[[...namespace]]/route.ts
@@ -1,18 +1,21 @@
 import {NextRequest, NextResponse} from "next/server";
-import {redis} from "@/context/redis";
-import {VectorWithData} from "@/lib/full-text-search/bm25";
+import {semanticSearch} from "@/context/semantic-search";
 
 export async function GET(req: NextRequest, { params }: { params?: { namespace: string[] } }): Promise<NextResponse> {
-    const keys: any = await req.json();
-    if (!keys) {
+    const payload: any = await req.json();
+    if (!payload) {
         return new NextResponse('Key is required', {status: 400});
     }
+    if(!('ids' in payload)) {
+        return new NextResponse('Payload must contain ids field', {status: 400});
+    }
+    const keys = payload.ids;
+    const includeMetadata = payload.includeMetadata;
+    const includeVectors = payload.includeVectors;
     const keysArray = (Array.isArray(keys) ? keys : [keys]).flatMap(key => typeof key === 'string' || typeof key === 'number' ? key : []);
     const namespace = params?.namespace.join('/') ?? "";
-    const pipeline = redis.pipeline();
-    keysArray.forEach(key => pipeline.json.get(namespace + '.' + key));
 
-    const documents = await pipeline.exec<VectorWithData[]>();
+    const documents = semanticSearch.fetch([keysArray, {includeMetadata, includeVectors}], {namespace});
 
     return NextResponse.json({result: documents});
 }

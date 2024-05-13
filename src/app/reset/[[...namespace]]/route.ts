9a8b9873f86fcd433efc8228ee704227f1fe8d38
as-celegen <ahmedselimcelegen@gmail.com>
2024-05-13 14:40:42 +0300

    Reset endpoint, Upstash Vector Endpoint Compatibility changes for namespace

--- src/app/reset/[[...namespace]]/route.ts
+++ src/app/reset/[[...namespace]]/route.ts
@@ -1,13 +1,15 @@
 import {fullTextSearch} from "@/context/full-text-search";
 import {NextRequest, NextResponse} from "next/server";
-import {redis} from "@/context/redis";
 import {semanticSearch} from "@/context/semantic-search";
 
-export async function DELETE(req: NextRequest): Promise<NextResponse> {
-    await redis.flushall();
-    await fullTextSearch.reset();
-    await semanticSearch.reset();
+export async function DELETE(req: NextRequest, { params }: { params?: { namespace: string[] } }): Promise<NextResponse> {
+    const namespace = params?.namespace.join('/') ?? "";
 
-    return new NextResponse('OK');
+    await Promise.all([
+        semanticSearch.reset({namespace}),
+        fullTextSearch.reset({namespace}),
+    ]);
+
+    return NextResponse.json({result: 'Success'});
 }
 

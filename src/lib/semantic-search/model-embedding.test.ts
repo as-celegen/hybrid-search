304a2b813a034022f45efbf396c6f3f9fe2301f3
as-celegen <ahmedselimcelegen@gmail.com>
2024-05-15 18:38:15 +0300

    Model Embedding tests updated

--- src/lib/semantic-search/model-embedding.test.ts
+++ src/lib/semantic-search/model-embedding.test.ts
@@ -1,14 +1,20 @@
 import { ModelEmbeddingSearch } from "@/lib/semantic-search/model-embedding";
 import {describe, it, expect, expectTypeOf} from "vitest";
+import {Index} from "@upstash/vector";
 
 const documents = [
-    {key: '1', title: 'Hello', document: 'Hello world'},
-    {key: '2', title: 'World', document: 'Hello world'},
-    {key: '3', title: 'Foo', document: 'lorem ipsum dolor sit amet'},
-    {key: '4', title: 'Bar', document: 'lorem ipsum world'},
-    {key: '5', title: 'Baz', document: 'Foo bar'},
-    {key: '6', title: 'Qux', document: 'Bar'},
+    {id: '1', data: 'Hello world', metadata: {title: 'Hello'}},
+    {id: '2', data: 'Hello world', metadata: {title: 'Hello'}},
+    {id: '3', data: 'lorem ipsum dolor sit amet', metadata: {title: 'lorem'}},
+    {id: '4', data: 'lorem ipsum world', metadata: {title: 'lorem'}},
+    {id: '5', data: 'Foo bar', metadata: {title: 'Foo'}},
+    {id: '6', data: 'Bar', metadata: {title: 'Bar'}},
 ];
+
+const index = new Index({
+    url: process.env.SEMANTIC_SEARCH_VECTOR_INDEX_URL,
+    token: process.env.SEMANTIC_SEARCH_VECTOR_INDEX_TOKEN,
+});
 
 describe('Model Embedding Search', () => {
     it('should create an instance', () => {
@@ -19,8 +25,17 @@ describe('Model Embedding Search', () => {
     it('should add documents', async () => {
         const search = new ModelEmbeddingSearch();
         const result = await search.upsert(documents);
-        expect(result).toBe(true);
-        const vector = await search.index.fetch(['1#ModelEmbedding', '2#ModelEmbedding'], {includeMetadata: true});
+        expect(result).toBe('Success');
+        const vector = await index.fetch(['1', '2'], {includeMetadata: true, namespace: '%20ModelEmbedding'});
+        expectTypeOf(vector).toBeArray();
+        expect(vector.filter(i => i !== null)).toHaveLength(2);
+    });
+
+    it('should add documents to namespaces', async () => {
+        const search = new ModelEmbeddingSearch();
+        const result = await search.upsert(documents, {namespace: 'test'});
+        expect(result).toBe('Success');
+        const vector = await index.fetch(['1', '2'], {includeMetadata: true, namespace: 'test%20ModelEmbedding'});
         expectTypeOf(vector).toBeArray();
         expect(vector.filter(i => i !== null)).toHaveLength(2);
     });
@@ -28,7 +43,7 @@ describe('Model Embedding Search', () => {
     it('should search documents', async () => {
         const search = new ModelEmbeddingSearch();
         await new Promise(resolve => setTimeout(resolve, 10000));
-        const results = await search.query('hello');
+        const results = await search.query({data: 'hello', topK: 3});
         expectTypeOf(results).toBeArray();
         console.log(results);
         expect(results.length).toBeGreaterThan(0);
@@ -38,15 +53,35 @@ describe('Model Embedding Search', () => {
         const search = new ModelEmbeddingSearch();
         await search.upsert(documents);
         const result = await search.delete('1');
-        expect(result).toBe(1);
-        const vector = await search.index.fetch(['1#ModelEmbedding']);
+        expect(result).toEqual({deleted: 1});
+        const vector = await index.fetch(['1'], {namespace: '%20ModelEmbedding'});
         expect(vector).toEqual([null]);
+    });
+
+    it('info should return namespaces', async () => {
+        const search = new ModelEmbeddingSearch();
+        const info = await search.info();
+        expect(Object.keys(info.namespaces).sort()).toEqual(['', 'test'].sort());
+    });
+
+    it('should list namespaces', async () => {
+        const search = new ModelEmbeddingSearch();
+        const namespaces = await search.listNamespaces();
+        expect(namespaces.sort()).toEqual(['', 'test'].sort());
+    });
+
+    it('should delete a namespace', async () => {
+        const search = new ModelEmbeddingSearch();
+        const result = await search.deleteNamespace('test');
+        expect(result).toBe('Success');
+        const namespaces = await search.listNamespaces();
+        expect(namespaces).toEqual(['']);
     });
 
     it('should reset the index', async () => {
         const search = new ModelEmbeddingSearch();
         await search.reset();
-        const result = await search.index.fetch(['1#ModelEmbedding']);
+        const result = await index.fetch(['1'], {namespace: '%20ModelEmbedding'});
         expect(result).toEqual([null]);
     });
 });

273170a93000f1aba5075a3ab6a35bcfe937cb0c
as-celegen <ahmedselimcelegen@gmail.com>
2024-05-15 03:29:28 +0300

    common test values in utils

---
+++ src/lib/hybrid-search/utils.test.ts
@@ -0,0 +1,80 @@
+export const result1 = [
+    {
+        id: 'a',
+        score: 0.7,
+        vector: [],
+        metadata: {
+            title: 'A',
+        }
+    },
+    {
+        id: 'b',
+        score: 0.8,
+        vector: [],
+        metadata: {
+            title: 'B',
+        }
+    },
+    {
+        id: 'c',
+        score: 0.9,
+        vector: [],
+        metadata: {
+            title: 'C',
+        }
+    },
+];
+
+export const result2 = [
+    {
+        id: 'a',
+        score: 0.6,
+        vector: [],
+        metadata: {
+            title: 'A',
+        }
+    },
+    {
+        id: 'b',
+        score: 0.7,
+        vector: [],
+        metadata: {
+            title: 'B',
+        }
+    },
+    {
+        id: 'c',
+        score: 0.8,
+        vector: [],
+        metadata: {
+            title: 'C',
+        }
+    },
+]
+
+export const result3 = [
+    {
+        id: 'a',
+        score: 0.8,
+        vector: [],
+        metadata: {
+            title: 'A',
+        }
+    },
+    {
+        id: 'b',
+        score: 0.7,
+        vector: [],
+        metadata: {
+            title: 'B',
+        }
+    },
+    {
+        id: 'c',
+        score: 0.6,
+        vector: [],
+        metadata: {
+            title: 'C',
+        }
+    },
+]

56b5656bb90e09bad6707edd1db3384dadae144d
as-celegen <ahmedselimcelegen@gmail.com>
2024-05-25 20:53:42 +0300

    css fix

--- src/components/search-box.tsx
+++ src/components/search-box.tsx
@@ -7,11 +7,11 @@ export default function SearchBox({ onSubmit, namespaces }: SearchBoxProps) {
     return (
         <form className="flex flex-row items-center justify-center w-full" onSubmit={onSubmit}>
             <label>Search: </label>
-            <input required type={"text"}/>
+            <input required type={"text"} className="bg-amber-100 text-blue-700"/>
             <label>Namespace: </label>
-            <select>
+            <select className="bg-amber-100 text-blue-700">
                 {namespaces.map((namespace) => (
-                    <option key={namespace} value={namespace}>{namespace.length == 0 ? <i>(Default)</i>: namespace}</option>
+                    <option key={namespace} value={namespace} className="bg-amber-100 text-blue-700">{namespace.length == 0 ? <i>(Default)</i>: namespace}</option>
                 ))}
             </select>
             <button type={"submit"}>Search</button>

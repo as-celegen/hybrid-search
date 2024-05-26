9d7eb0163db744869499352362b743526fe60d02
as-celegen <ahmedselimcelegen@gmail.com>
2024-05-26 20:47:10 +0300

    upsert waitUntil for keeping process running while BM25 upsert is going

--- src/app/upsert-data/[[...namespace]]/route.ts
+++ src/app/upsert-data/[[...namespace]]/route.ts
@@ -2,6 +2,9 @@ import { fullTextSearch } from '@/context/full-text-search';
 import {semanticSearch} from "@/context/semantic-search";
 import {NextRequest, NextResponse} from "next/server";
 import {VectorWithData} from "@/lib/full-text-search/bm25";
+import {waitUntil} from "@vercel/functions";
+
+export const maxDuration = 60;
 
 export async function POST(req: NextRequest, { params }: { params?: { namespace: string[] } }): Promise<NextResponse> {
     const body = await req.json();
@@ -15,7 +18,8 @@ export async function POST(req: NextRequest, { params }: { params?: { namespace:
     }
     const documents: VectorWithData[] = Array.isArray(body) ? body : [body];
     const namespace = params?.namespace?.join('/') ?? "";
-    await Promise.all([semanticSearch.upsert(documents, {namespace}), fullTextSearch.upsert(documents, {namespace})]);
+    await semanticSearch.upsert(documents, {namespace});
+    waitUntil(fullTextSearch.upsert(documents, {namespace}));
 
     return NextResponse.json({result: 'Success'});
 }
